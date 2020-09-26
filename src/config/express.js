4fc92cdb45b633ee534de6f1d94d480aa90e7d87
Ian Alves <ian.alves21@gmail.com>
2020-09-26 16:50:16 -0400

    adding log midleware to all requests

--- src/config/express.js
+++ src/config/express.js
@@ -8,14 +8,19 @@ dotenv.config({
     path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
 });
 
-const router = require('../routes');
+const logMiddleware = (request, response, next) => {
+    console.log("\x1b[32m%s\x1b[0m",`REQUESTED METHOD [${request.method}] ON URL [${request.url}]`);
+    next();
+};
 
+const router = require('../routes');
 const app = express();
 
 consign()
     .include('./src/controllers')
     .into(app);
 
+app.use(logMiddleware);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
